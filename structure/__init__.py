ba43f0666bafb8f4089ed93df86518af352dff53
xxhddt163 <56008174@qq.com>
2020-09-15 10:42:25 +0800

    栈、队列、双端队列类

--- structure/__init__.py
+++ structure/__init__.py
@@ -1,5 +1,6 @@
 class Stack:
     """构建栈"""
+
     def __init__(self):
         self.items = []
 
@@ -26,6 +27,7 @@ class Stack:
 
 class Queue:
     """构建队列"""
+
     def __init__(self):
         self.item = []
 
@@ -44,3 +46,30 @@ class Queue:
     def size(self):
         """返回队列的元素个数"""
         return len(self.item)
+
+
+class Deque:
+    """双端队列"""
+
+    def __init__(self):
+        self.items = []
+
+    def addFront(self, item):
+        """在队首加入元素"""
+        self.items.append(item)
+
+    def removeFront(self):
+        """弹出队首元素"""
+        return self.items.pop()
+
+    def addRear(self, item):
+        """队尾加入元素"""
+        self.items.insert(0, item)
+
+    def removeRear(self):
+        """队尾移除元素"""
+        return self.items.pop(0)
+
+    def isEmpty(self):
+        """返回队列是否为空"""
+        return self.items == []
