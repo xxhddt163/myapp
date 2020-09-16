6e5cdde2413e85861198297fd6d82f895b8f920d
xxh <56008174@qq.com>
2020-09-16 22:29:06 +0800

    考勤设置文件

---
+++ office/setting.py
@@ -0,0 +1,10 @@
+class Setting:
+    def __init__(self):
+        self.uptime = '09:00'
+        self.downtime = '18:30'
+
+        self.uptime_bus = '09:00'
+        self.downtime_bus = '20:00'
+
+        self.uptime_bus2 = '10:00'
+        self.downtime_bus2 = '21:00'
