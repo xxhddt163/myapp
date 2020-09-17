bd275415974e10a119d0c8863149116e9e09cb50
xxh <56008174@qq.com>
2020-09-18 00:07:23 +0800

    主文件

--- office/main.py
+++ office/main.py
@@ -1,6 +1,7 @@
 import datetime
 import easygui
 import openpyxl
+import copy
 from myapp.office.setting import Setting
 from openpyxl.comments import Comment  # 批注相关的模块
 from openpyxl import styles
@@ -20,20 +21,57 @@ def main():
                     up_time, down_time = crow_time(row, number)  # 上、下班时间
                     if department[sheet_work['K' + str(number)].value] != "招商运营部":
                         check_time(up_time, down_time, trunk_line, row, number)
+                else:
+                    continue
         else:
             break
     save_file()
 
 
+def find_line(name):
+    """在汇总表找到名字对应的行"""
+    for line in range(4, 100):
+        if sheet_trunk['D' + str(line)].value is not None and sheet_trunk['D' + str(line)].value == name:
+            return line
+
+
+def crow_time(row, number):
+    """遍历考勤"""
+    return sheet_work[converttotitle(row) + str(number + 1)].value[:5], \
+           sheet_work[converttotitle(row) + str(number + 1)].value[-5:]
+
+
 def check_time(up_time, down_time, trunk_line, row, number):
     """非招商运营部检查考勤"""
-    if up_time <= time_set.uptime and down_time >= time_set.downtime:  # 正常考勤
-        sheet_trunk[converttotitle(row + 4) + str(trunk_line)].value = '√ '
-    elif up_time > time_set.uptime and down_time >= time_set.downtime:  # 迟到
-        sheet_trunk[converttotitle(row + 4) + str(trunk_line)].value = \
-            sheet_work[converttotitle(row) + str(number + 1)].value
-        up_cell_format(row, trunk_line)
-        up_comment(row, trunk_line, up_time, down_time)
+    if trunk_line is None:  # 考勤表中出现的人名未出现在统计表中
+        print(f"{sheet_work['K' + str(number)].value}未出现在考勤统计表中")
+        choose = input("是否继续统计？(y/n)")
+        if choose.lower() == "y":
+            return None
+        else:
+            raise TypeError
+
+    uptime_hour = int(up_time.split(":")[0])
+    up_min = int(up_time.split(":")[1])
+    set_up_hour = int(time_set.uptime.split(":")[0])
+    set_up_min = int(time_set.uptime.split(":")[1])
+    down_hour = int(down_time.split(":")[0])
+    downtime_min = int(down_time.split(":")[1])
+    set_down_hour = int(time_set.downtime.split(":")[0])
+    set_down_min = int(time_set.downtime.split(":")[1])
+
+    if uptime_hour < set_up_hour or uptime_hour == set_up_hour and up_min <= set_up_min:  # 判定上班时间
+        if down_hour == set_down_hour and downtime_min >= set_down_min or down_hour > set_down_hour:  # 判定下班时间
+            sheet_trunk[converttotitle(row + 4) + str(trunk_line)].value = "√"  # 正常考勤打钩
+    else:
+        if up_time != down_time and down_hour - uptime_hour >= 8:
+            sheet_trunk[converttotitle(row + 4) + str(trunk_line)].value = \
+                sheet_work[converttotitle(row) + str(number + 1)].value
+            up_cell_format(row, trunk_line)  # 上班异常字体颜色改红
+            up_comment(row, trunk_line, up_time, down_time)  # 统计迟到时间并加入批注
+        else:
+            sheet_trunk[converttotitle(row + 4) + str(trunk_line)].value = \
+                sheet_work[converttotitle(row) + str(number + 1)].value
 
 
 def up_comment(row, trunk_line, up_time, down_time):
@@ -57,19 +95,6 @@ def up_cell_format(row, trunk_line):
     """设置上班异常考勤单元格文字颜色"""
     sheet_trunk[converttotitle(row + 4) + str(trunk_line)].font = \
         styles.Font(name='Aria', size=8, color='00FF0000')  # 不正常上班考勤填充红色
-
-
-def crow_time(row, number):
-    """遍历考勤"""
-    return sheet_work[converttotitle(row) + str(number + 1)].value[:5], \
-           sheet_work[converttotitle(row) + str(number + 1)].value[-5:]
-
-
-def find_line(name):
-    """在汇总表找到名字对应的行"""
-    for line in range(4, 100):
-        if sheet_trunk['D' + str(line)].value is not None and sheet_trunk['D' + str(line)].value == name:
-            return line
 
 
 def converttotitle(n):
