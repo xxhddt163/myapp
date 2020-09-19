70b1f9caa2f9e2d90110a7e31dec814548746d20
xxh <56008174@qq.com>
2020-09-19 14:55:28 +0800

    自动填写批注与迟到次数

--- office/remark.py
+++ office/remark.py
@@ -8,31 +8,28 @@ import re
 class Remark:
     """根据批注自动填写迟到次数并填写备注"""
 
-    def __init__(self, file, sh_name):
-        self.file = file
-        self.sh_name = sh_name
-        self.open_file()
-        self.iter_row()
-        self.delete_rstrip()
-        self.save_file()
-
-    def open_file(self):
-        """打开excel文件"""
+    def __init__(self, path, sheet):
+        self.file = path
+        self.sh_name = sheet
         self.excel = openpyxl.load_workbook(file)
         self.sheet = self.excel[self.sh_name]
+        self.iter_row()
+        self.del_strip()
+        self.save_file()
 
     def iter_row(self):
         """遍历列"""
         for number in range(4, 100):
             if self.sheet['D' + str(number)].value is not None:
+                print(f"正在统计{self.sheet['D' + str(number)].value}是否有迟到")
                 self.iter_line(number)
             else:
                 break
 
     def iter_line(self, number):
-        """遍历Dnumber有内容的行"""
+        """遍历所有考勤"""
         for line in range(5, 36):
-            row = self.converttotitle(line)
+            row = self.convert2title(line)
             if self.sheet[row + str(number)].comment is not None:
                 self.remark(number, line, row)
 
@@ -41,31 +38,31 @@ class Remark:
         info = self.sheet[row + str(number)].comment
         temp = re.search(r"考勤异常,上班打卡时间.*,下班打卡时间.*,迟到(\d+)小时(\d+)分钟", info.content)  # 提取迟到时间的正则
         hour = temp.group(1)  # 提取迟到小时
-        min = temp.group(2)  # 提取迟到分钟
+        minute = temp.group(2)  # 提取迟到分钟
         if int(hour) == 0:
             if self.sheet['BI' + str(number)].value is not None:
                 self.sheet['BI' + str(number)].value = self.sheet['BI' + str(number)].value + \
                                                        str(datetime.datetime.now().month - 1) + \
                                                        "月" + str(line - 4) + "日" \
-                                                       + "迟到" + str(min) + "分钟,"
-                self.check_time(number, hour, min)
+                                                       + "迟到" + str(minute) + "分钟,"
+                self.check_time(number, hour, minute)
             else:
                 self.sheet['BI' + str(number)].value = str(datetime.datetime.now().month - 1) + \
                                                        "月" + str(line - 4) + "日" \
-                                                       + "迟到" + str(min) + "分钟,"
-                self.check_time(number, hour, min)
+                                                       + "迟到" + str(minute) + "分钟,"
+                self.check_time(number, hour, minute)
         else:
             if self.sheet['BI' + str(number)].value is not None:
                 self.sheet['BI' + str(number)].value = self.sheet['BI' + str(number)].value + \
                                                        str(datetime.datetime.now().month - 1) + \
                                                        "月" + str(line - 4) + "日" \
-                                                       + "迟到" + str(hour) + "小时" + str(min) + "分钟,"
-                self.check_time(number, hour, min)
+                                                       + "迟到" + str(hour) + "小时" + str(minute) + "分钟,"
+                self.check_time(number, hour, minute)
             else:
                 self.sheet['BI' + str(number)].value = str(datetime.datetime.now().month - 1) + \
                                                        "月" + str(line - 4) + "日" \
-                                                       + "迟到" + str(hour) + "小时" + str(min) + "分钟,"
-                self.check_time(number, hour, min)
+                                                       + "迟到" + str(hour) + "小时" + str(minute) + "分钟,"
+                self.check_time(number, hour, minute)
         self.sheet['BI' + str(number)].font = styles.Font(name='宋体', size=6, bold=True)
 
     def save_file(self):
@@ -74,19 +71,19 @@ class Remark:
                                         filetypes=["*.xlsx"])
         self.excel.save(file_save)
 
-    def check_time(self, number, hour, min):
+    def check_time(self, number, hour, minute):
         """统计迟到次数"""
         if int(hour) >= 1:  # 大于一个小时的迟到时间
             letter = "BB"
             self.check_time2(number, letter)
         elif int(hour) == 0:
-            if int(min) < 10:  # 迟到10分钟以内
+            if int(minute) < 10:  # 迟到10分钟以内
                 letter = "AY"
                 self.check_time2(number, letter)
-            elif 10 <= int(min) < 30:
+            elif 10 <= int(minute) < 30:
                 letter = "AZ"
                 self.check_time2(number, letter)
-            elif 30 <= int(min) < 60:
+            elif 30 <= int(minute) < 60:
                 letter = "BA"
                 self.check_time2(number, letter)
 
@@ -98,24 +95,24 @@ class Remark:
             self.sheet[letter + str(number)].value = int(self.sheet[letter + str(number)].value) + 1
         self.sheet[letter + str(number)].font = styles.Font(name='宋体', size=6, bold=True)
 
-    def delete_rstrip(self):
+    def del_strip(self):
         """删除每一行备注的最后一个逗号"""
         for number in range(4, 91):
             if self.sheet['BI' + str(number)].value is not None:
                 self.sheet['BI' + str(number)].value = self.sheet['BI' + str(number)].value.rstrip(',')
 
     @staticmethod
-    def converttotitle(n):
+    def convert2title(n):
         """将数字转换成EXCEL列"""
-        rStr = ""
+        title = ""
         while n != 0:
             res = n % 26
             if res == 0:
                 res = 26
                 n -= 26
-            rStr = chr(ord('A') + res - 1) + rStr
+            title = chr(ord('A') + res - 1) + title
             n = n // 26
-        return rStr
+        return title
 
 
 if __name__ == '__main__':
