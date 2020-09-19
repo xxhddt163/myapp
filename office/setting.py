856362454078505a94a70ea145b51df28cebc332
xxh <56008174@qq.com>
2020-09-19 14:14:35 +0800

    考勤时间时间设置文件

--- office/setting.py
+++ office/setting.py
@@ -1,8 +1,10 @@
 class Setting:
     def __init__(self):
+        # 其他部门上班时间
         self.uptime = '09:00'
         self.downtime = '18:30'
 
+        # 招商运营部2个上下班时间
         self.uptime_bus = '09:00'
         self.downtime_bus = '20:00'
 
