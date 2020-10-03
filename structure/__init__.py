1214ed0ba31594be55a8d7c9eb5443f4ddd92a7d
xxh <56008174@qq.com>
2020-10-04 00:05:41 +0800

    链表

--- structure/__init__.py
+++ structure/__init__.py
@@ -82,8 +82,8 @@ class Deque:
 class Node:
     """链表的节点"""
 
-    def __init__(self, initdata):
-        self.data = initdata
+    def __init__(self, init_data):
+        self.data = init_data
         self.next = None
 
     def getData(self):
@@ -92,15 +92,15 @@ class Node:
     def getNext(self):
         return self.next
 
-    def setData(self, newdata):
-        self.data = newdata
+    def setData(self, new_data):
+        self.data = new_data
 
-    def setNext(self, newnext):
-        self.next = newnext
+    def setNext(self, new_next):
+        self.next = new_next
 
 
-class UnorderList:
-    """链表"""
+class UnorderedList:
+    """无序链表"""
 
     def __init__(self):
         self.head = None
@@ -149,3 +149,78 @@ class UnorderList:
             self.head = current.getNext()
         else:
             previous.setNext(current.getNext())
+
+
+class OrderedList:
+    """有序链表"""
+
+    def __init__(self):
+        self.head = None
+
+    def add(self, item):
+        """向链表表头添加元素"""
+        current = self.head
+        previous = None
+        stop = False
+        while current is not None and not stop:
+            if current.getData() > item:
+                stop = True
+            else:
+                previous = current
+                current = current.getNext()
+
+        temp = Node(item)
+        if previous is None:
+            temp.setNext(self.head)
+            self.head = temp
+        else:
+            temp.setNext(current)
+            previous.setNext(temp)
+
+    def size(self):
+        """获取链表的节点个数"""
+        current = self.head
+        count = 0
+        while current is not None:
+            count += 1
+            current = current.getNext()
+
+        return count
+
+    def search(self, item):
+        """检查目标元素是否在链表内"""
+        found = False
+        current = self.head
+        stop = False
+        while current is not None and not found and not stop:
+            if current.getData() > item:
+                stop = True
+            if current.getData() == item:
+                found = True
+            else:
+                current = current.getNext()
+
+        return found
+
+    def remove(self, item):
+        """删除列表中的指定元素"""
+        current = self.head  # 用来存放当前节点的指向
+        previous = None  # 用来存放上一个节点的指向
+        found = False
+        while not found:
+            if current.getData() == item:
+                found = True
+            else:
+                previous = current
+                current = current.getNext()
+
+        if previous is None:
+            self.head = current.getNext()
+        else:
+            previous.setNext(current.getNext())
+
+
+test = OrderedList()
+test.add(5)
+test.add(6)
+print(test.size())
