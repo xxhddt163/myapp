34ba310d7f460516444a61d9f1d2dc4f5c1dbb47
xxh <56008174@qq.com>
2020-11-02 01:23:37 +0800

    v0.2

--- Schedule/main.py
+++ Schedule/main.py
@@ -1,27 +1,73 @@
 import openpyxl
 import easygui
-import re
 import pickle
+from openpyxl.comments import Comment
+from openpyxl import styles
+import random
+
+"""
+日志功能
+罗列候选名单功能
+"""
 
 
 def main(excel):
     for sheet_name in excel.sheetnames:  # 遍历课表
         for n1 in range(2, 7):
             for n2 in range(4, 12):
-                row = convert2title(n1)
+                row = convert2title(n1)  # 数字转换为对应的列
                 if excel[sheet_name][row + str(n2)].value is not None:
-                    if excel[sheet_name][row + str(n2)].value.replace('\r', '').split("\n")[1] in person_name:
+                    teacher_name = excel[sheet_name][row + str(n2)].value.replace('\r', '').split("\n")[1]  # 单元格中教师名
+                    if teacher_name in person_name:
                         info = teacher_info.copy()  # 教师信息备份
                         subject_name = excel[sheet_name][row + str(n2)].value.replace('\r', '').split("\n")[
                             0]  # 单元格中科目名
                         grade = sheet_name[:3]  # 年级
                         info1 = filter_1(subject_name, info)  # 能上指定课程的教师信息字典
                         info2 = filter_2(row + str(n2), excel, info1)  # 能上指定课程并且课程不冲突的教师信息
-                        final_info = init_grade(grade, info2)  # 最终的教师名单
-                        #print(f"{info2} {sheet_name} {row + str(n2)}")
-                        #print(f"{final_info} {sheet_name} {row + str(n2)}")
-                        excel[sheet_name][row + str(n2)].value = subject_name + '\n' + final_info[0]
-                        excel.save('test.xlsx]')
+                        if len(info2) != 0:
+                            final_info = init_grade(grade, info2)  # 最终的教师名单
+                            # print(f"{info2} {sheet_name} {row + str(n2)}")
+                            # print(f"{final_info} {sheet_name} {row + str(n2)}")
+                            final_name = random_name(final_info)  # 代课老师
+                            comment(excel[sheet_name][row + str(n2)],
+                                    teacher_name, final_name)
+                            excel[sheet_name][row + str(n2)].value = subject_name + '\n' + final_name
+                            cell_format(excel[sheet_name][row + str(n2)])
+                        else:
+                            # print(f"{info2} {sheet_name} {row + str(n2)}")
+                            excel[sheet_name][row + str(n2)].value = subject_name + '\n' + "无候选教师"
+                            cell_format1(excel[sheet_name][row + str(n2)])
+    excel.save('test.xlsx')
+
+
+def log():
+    """日志"""
+    pass
+
+
+def random_name(teacher_name):
+    """在候选名单中随机选一名教师代课"""
+    end = len(teacher_name) - 1
+    return teacher_name[random.randint(0, end)]
+
+
+def comment(cell, old_teacher, new_teacher):
+    """修改过的表格自动填入批注"""
+    cache = Comment(f"初始教师：{old_teacher}\n代课教师：{new_teacher}", "")
+    cache.width, cache.height = 120, 120
+    cell.comment = cache
+
+
+def cell_format(cell):
+    """修改单元格字体颜色"""
+    cell.font = styles.Font(color='0099CC00')  # 不正常上班考勤填充绿色
+
+
+def cell_format1(cell):
+    """修改单元格字体颜色"""
+    cell.font = styles.Font(color='003366FF')  # 无代课教师填充蓝色
+
 
 def init_grade(object_grade, info):
     """筛选出最终候选名单"""
@@ -70,8 +116,9 @@ def filter_1(subject, info):
 def filter_2(cell, excel, info):
     """便利每个单元格的对应单元格筛选出闲置教师"""
     for sheet_name in excel.sheetnames:
-        if excel[sheet_name][cell].value.replace('\r', '').split("\n")[1] in info:
-            info.pop(excel[sheet_name][cell].value.replace('\r', '').split("\n")[1])
+        if excel[sheet_name][cell].value is not None:
+            if excel[sheet_name][cell].value.replace('\r', '').split("\n")[1] in info:
+                info.pop(excel[sheet_name][cell].value.replace('\r', '').split("\n")[1])
     return info
 
 
