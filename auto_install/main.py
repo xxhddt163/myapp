8705904b49243e1a521c9cb649ee2951e1f3a49a
xxh <56008174@qq.com>
2020-11-29 23:32:04 +0800

    Pywinauto自动安装windows软件

---
+++ auto_install/main.py
@@ -0,0 +1,54 @@
+from pywinauto import Application
+import time
+import os
+
+"""
+QQ:
+1："腾讯QQ安装向导"
+2：title="自定义选项", class_name="Button"
+3: title="添加到快速启动栏", class_name="Button"
+4：title="开机自动启动", class_name="Button"
+5：title="C:\Program Files (x86)\Tencent\QQ", class_name="Edit"
+6: title="立即安装", class_name="Button"
+"""
+
+
+class Setup:
+    """自动安装软件"""
+
+    def __init__(self):
+        self.step = {"QQ": ["自定义选项",
+                            "添加到快速启动栏",
+                            "开机自动启动",
+                            "C:\Program Files (x86)\Tencent\QQ",
+                            "立即安装",
+                            "完成安装"]}
+        self.app = Application(backend='win32').start(r"C:\Users\admin\Desktop\PCQQ2020.exe")
+        self.main_window = ''
+        self.main()
+
+    def check_window(self, volume1):
+        """检测窗口是否已经加载"""
+        while True:
+            if self.main_window.window(title=volume1).wait('enabled', timeout=300):  # 等待指定按钮出现
+                return self.main_window.child_window(title=volume1)
+            time.sleep(1)
+
+    def main(self):
+        if self.app.window(title="腾讯QQ安装向导").wait('enabled', timeout=300):  # 等待界面出现才执行
+            self.main_window = self.app["腾讯QQ安装向导"]
+            self.run()
+
+    def run(self):
+        """执行命令安装程序"""
+        for each in self.step['QQ']:
+            if ":" not in each:
+                cache = self.check_window(each)
+                cache.click()
+            elif ":" in each:
+                cache = self.check_window(each)
+                cache.set_text(each.replace('C', 'D'))
+
+
+if __name__ == '__main__':
+    s = Setup()
