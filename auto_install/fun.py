e675ba959c02cce56addf55d5c9820c82c230bc0
xxhddt163 <56008174@qq.com>
2020-11-30 18:49:00 +0800

    对象

--- auto_install/fun.py
+++ auto_install/fun.py
@@ -7,6 +7,7 @@ class Program:
     def __init__(self, program_path, program_name, program_type, main_window):
         """实例化应用对象，实例化主窗口文件"""
         self.app = Application(backend=program_type).start(program_path + "\\" + program_name)
+        self.program_name = program_name
         while True:
             if self.app.window(title=main_window).wait("ready", timeout=300):
                 self.main_window = self.app[main_window]
@@ -17,7 +18,23 @@ class Program:
         if self.main_window.window(title_re=obj_name).wait("ready", timeout=300):
             self.main_window = self.main_window.child_window(title=obj_name)
 
+    def button_click(self):
+        """按下指定按钮"""
+        self.main_window.click_input()
+
+    def edit_text_win32(self):
+        """编辑edit win32类程序"""
+        path = r"D:\Program Files"
+        self.main_window.set_text(path + "\\" + self.program_name)
+
+    def edit_text_uia(self):
+        path = r"D:\Program Files"
+        self.main_window.click()
+        time.sleep(.5)
+        self.main_window.type_keys("%a")
+        time.sleep(.5)
+        self.main_window.type_keys(path + "\\" + self.program_name)
+
 
 p = Program(os.getcwd(), "Wechat", "uia", "微信安装向导")
-pass
-pass
+p.check_window()
