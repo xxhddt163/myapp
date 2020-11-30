23bbf9027093cc9dc4f07954f0c9b188865191ab
xxhddt163 <56008174@qq.com>
2020-11-30 18:37:39 +0800

    对象

--- auto_install/fun.py
+++ auto_install/fun.py
@@ -1,13 +1,23 @@
 from pywinauto import Application
 import time
-from os import path
+import os
 
 
 class Program:
-    def __init__(self, program_path, program_name, program_type):
+    def __init__(self, program_path, program_name, program_type, main_window):
+        """实例化应用对象，实例化主窗口文件"""
         self.app = Application(backend=program_type).start(program_path + "\\" + program_name)
+        while True:
+            if self.app.window(title=main_window).wait("ready", timeout=300):
+                self.main_window = self.app[main_window]
+            time.sleep(1)
+
+    def check_window(self, obj_name):
+        """检测目标对象是否就绪"""
+        if self.main_window.window(title_re=obj_name).wait("ready", timeout=300):
+            self.main_window = self.main_window.child_window(title=obj_name)
 
 
-p = Program(r"C:\Users\admin\Desktop\soft", "QQ", "win32")
+p = Program(os.getcwd(), "Wechat", "uia", "微信安装向导")
 pass
 pass
