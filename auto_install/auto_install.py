0ed4993503fa634d17a94726b102d36da6976f9d
xxh <56008174@qq.com>
2021-01-03 21:43:48 +0800

    Pywinauto自动安装windows软件

--- auto_install/auto_install.py
+++ auto_install/auto_install.py
@@ -222,7 +222,7 @@ if __name__ == '__main__':
             continue
 
         if each == "WPS":
-            install_from_image(each, 'wpssetup_k56008174_281235.exe', 8, 0.9, 8)
+            install_from_image(each, 'wpssetup_k56008174_281235.exe', 8, 0.9, 2)
             continue
 
         if each == "163music":
